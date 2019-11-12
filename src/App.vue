9a043aeb7a74b44b6d3fab921ae960eb49dcfab0
longwind <springautumn@qq.com>
2019-11-12 11:14:23 +0800

    npm method

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div class="chart">
+    <s-chart :data="lineData" :data-fields="dataFields" :size="[700,600]">
+      <s-line :attrs="attrs" />
+      <s-axis :attrs="{orient:'left'}" />
+      <s-axis :attrs="{orient:'bottom'}" />
+      <s-tooltip :attrs="tooltipAttrs" />
+    </s-chart>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+export default {
+  data: function() {
+    return {
+      attrs: {
+        pos: ["10%", "10%"],
+        size: ["80%", "80%"]
+      },
+      lineData: [
+        { date: "05-01", catgory: "图例一", sales: 15.2 },
+        { date: "05-02", catgory: "图例一", sales: 39.2 },
+        { date: "05-03", catgory: "图例一", sales: 31.2 },
+        { date: "05-04", catgory: "图例一", sales: 65.2 },
+        { date: "05-05", catgory: "图例一", sales: 55.2 },
+        { date: "05-06", catgory: "图例一", sales: 75.2 },
+        { date: "05-07", catgory: "图例一", sales: 95.2 },
+        { date: "05-08", catgory: "图例一", sales: 65.2 }
+      ],
+      tooltipAttrs: {
+        formatter: data => `${data.date}-${data.catgory}-${data.sales}`
+      },
+      dataFields: {
+        row: "catgory",
+        value: "sales",
+        text: "date"
+      }
+    };
   }
+};
+</script>
+<style>
+.chart {
+  height: 600px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import * as CatChartsVue from "cat-charts-vue";
+Vue.use(CatChartsVue);
 Vue.config.productionTip = false;
 
 new Vue({
